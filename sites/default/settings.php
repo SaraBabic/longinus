fd125dccff60deefddd2685c7aa4e0db3a72b686
SaraBabic <sarababic0101@gmail.com>
2023-08-16 07:49:37 +0200

    Add Trusted Host Settings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -763,6 +763,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
  */
 # $settings['trusted_host_patterns'] = [];
+$settings['trusted_host_patterns'] = ['.*'];
 
 /**
  * The default list of directories that will be ignored by Drupal's file API.
