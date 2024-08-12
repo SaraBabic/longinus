fe20c5846d02dbeeb186208d272276cbbf1246a8
Goran Nikolovski <nikolovski84@gmail.com>
2024-08-12 15:25:23 +0200

    Create backup-database.sh

---
+++ backup-database.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Usage: sh backup-database.sh longinus.rs "gn-master-backups/Servers/GN Digital/live-pp/promo-ponude.com/databases/";
+
+if [ -z "$1" ]
+  then
+    echo "Please provide backup name."
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "Please provide S3 path."
+    exit 1
+fi
+
+suffix=$(date '+%Y-%m-%d_%H-%M-%S')
+"/var/www/$1/bin/drush" sql-dump --gzip --structure-tables-key=common > "/var/www/$1/$1-$suffix.sql"
+aws s3 cp "/var/www/$1/$1-$suffix.sql" "s3://$2"
+rm "/var/www/$1/$1-$suffix.sql"
